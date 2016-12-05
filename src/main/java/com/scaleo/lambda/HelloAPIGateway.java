801c956015e95e916c5fd5c6c354df25bc67e2ff
David Galichet <david.galichet@gmail.com>
2016-12-05 23:28:01 +0100

    Add logs in lambda

--- src/main/java/com/scaleo/lambda/HelloAPIGateway.java
+++ src/main/java/com/scaleo/lambda/HelloAPIGateway.java
@@ -3,10 +3,15 @@ package com.scaleo.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+import java.util.Date;
+
 public class HelloAPIGateway {
+
+    private Date containerCreation  = new Date();
 
     public String helloHandler(Person person, Context context) {
         LambdaLogger logger = context.getLogger();
+        logger.log("This container has been created at " + containerCreation.toString());
         logger.log("Received a Hello from " + person);
 
         return "Hi " + person.getFirstname() + " " + person.getLastname();
