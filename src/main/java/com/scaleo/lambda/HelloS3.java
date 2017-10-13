0ed4466076eefc3e9502b5610ac21cc4d0abe712
David Galichet <david.galichet@gmail.com>
2017-10-13 14:48:37 +0200

    fix import

--- src/main/java/com/scaleo/lambda/HelloS3.java
+++ src/main/java/com/scaleo/lambda/HelloS3.java
@@ -2,6 +2,7 @@ package com.scaleo.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.scaleo.lambda.image.ImageProcessor;
 
 public class HelloS3 {
@@ -9,7 +10,7 @@ public class HelloS3 {
     public void s3EventHandler(S3Event s3Event, Context context) {
         // TODO code here...
 
-        //LambdaLogger logger = context.getLogger();
-        //ImageProcessor.createThumbnail(s3Event, logger)
+        LambdaLogger logger = context.getLogger();
+        ImageProcessor.createThumbnail(s3Event, logger);
     }
 }
